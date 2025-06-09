c7e4d4c889a692c2a2dde1985e52586e8bfe7346
codingnekochan <mariam@mycantant.com>
2025-06-09 21:36:12 +0100

    merge

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,56 +1,58 @@
-import AddButton from '@/components/buttons/AddButton';
-import GetScriptSafeCard from '@/components/card/GetScriptSafeCard';
-import Container from '@/components/common/Container';
-import MainPageHeader from '@/components/headers/MainPageHeader';
-import { homeContent } from '@/constants/contentProps';
-import { LinearGradient } from 'expo-linear-gradient';
-import { router } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import AddButton from "@/components/buttons/AddButton";
+import GetScriptSafeCard from "@/components/card/GetScriptSafeCard";
+import Container from "@/components/common/Container";
+import MainPageHeader from "@/components/headers/MainPageHeader";
+import { homeContent } from "@/constants/contentProps";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
 
 const HomeScreen = () => {
+	const handleClick = () => {
+		router.push("/stepOne");
+	};
+	return (
+		<LinearGradient
+			colors={["#E0EDFF", "#E4EFFF", "#E7F0FF"]}
+			style={{ flex: 1 }}
+			start={{ x: 0, y: 0 }}
+			end={{ x: 0, y: 1 }}
+		>
+			<Container bg="transparent">
+				<MainPageHeader />
+				<View className="flex-1">
+					<View className="bg-white pt-5 pb-7 rounded-xl px-6 border-[#E8F3FF] border">
+						<Text className="font-mediumSFDisplay text-base text-[#333333] text-center">
+							Verify your prescription in less than a minute
+						</Text>
+						<View className="mt-6 ">
+							<AddButton onPress={handleClick} />
+						</View>
+					</View>
+					<View className="mt-9 mb-9">
+						<Text className="font-boldSFDisplay text-base text-[#3333333] leading-[32px]">
+							Get more from ScriptSafe
+						</Text>
+						<View className="mt-5 gap-6">
+							{homeContent.map((content, index) => {
+								const Icon = content.icon;
+								return (
+									<GetScriptSafeCard
+										key={index}
+										id={content.id}
+										text={content.text}
+										subtext={content.subtext}
+										icon={<Icon />}
+									/>
+								);
+							})}
+						</View>
+					</View>
+				</View>
+			</Container>
+		</LinearGradient>
+	);
+};
 
-    const handleClick = () => {
-        router.push('/stepOne');
-    }
-    return (
-        <LinearGradient colors={['#E0EDFF', '#E4EFFF', '#E7F0FF']} style={{ flex: 1 }} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} >
-            <Container bg='transparent'>
-                <MainPageHeader />
-                <View className='flex-1'>
-                    <View className='bg-white pt-5 pb-7 rounded-xl px-6 border-[#E8F3FF] border'>
-                        <Text className='font-mediumSFDisplay text-base text-[#333333] text-center'>
-                            Verify your prescription in less than a minute
-                        </Text>
-                        <View className='mt-6 '>
-                            <AddButton onPress={handleClick} />
-                        </View>
-                    </View>
-                    <View className='mt-9 mb-9'>
-                        <Text className='font-boldSFDisplay text-base text-[#3333333] leading-[32px]'>
-                            Get more from ScriptSafe
-                        </Text>
-                        <View className='mt-5 gap-6'>
-                            {
-                                homeContent.map((content, index) => {
-                                    const Icon = content.icon;
-                                    return <GetScriptSafeCard
-                                        key={index}
-                                        id={content.id}
-                                        text={content.text}
-                                        subtext={content.subtext}
-                                        icon={<Icon />}
-                                    />
-
-                                })
-                            }
-                        </View>
-                    </View>
-
-                </View>
-            </Container>
-        </LinearGradient>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
--- screens/Presecription/PrescriptionOverviewScreen.tsx
+++ screens/Presecription/PrescriptionOverviewScreen.tsx
@@ -1,88 +1,96 @@
-import AddButton from '@/components/buttons/AddButton'
-import RecentPrescriptionCard from '@/components/card/RecentPrescriptionCard'
-import Container from '@/components/common/Container'
-import MainPageHeader from '@/components/headers/MainPageHeader'
-import { recentPrescriptions } from '@/constants/contentProps'
-import { img_prescription_empty } from '@/constants/images'
-import { usePrescriptionStore } from '@/states/prescription'
-import { LinearGradient } from 'expo-linear-gradient'
-import { router } from 'expo-router'
-import React, { useState } from 'react'
-import { Image, Text, View } from 'react-native'
+import AddButton from "@/components/buttons/AddButton";
+import RecentPrescriptionCard from "@/components/card/RecentPrescriptionCard";
+import Container from "@/components/common/Container";
+import MainPageHeader from "@/components/headers/MainPageHeader";
+import { recentPrescriptions } from "@/constants/contentProps";
+import { img_prescription_empty } from "@/constants/images";
+import { usePrescriptionStore } from "@/states/prescription";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import React from "react";
+import { Image, Text, View } from "react-native";
 
 const PrescriptionOverviewScreen = () => {
-    const clickPrescription = usePrescriptionStore((state: any) => state.prescriptionState)
+	const clickPrescription = usePrescriptionStore(
+		(state: any) => state.prescriptionState
+	);
 
-    const handleClick = () => {
-        router.push('/stepOne')
-    }
-    return (
-        <>
-            {!clickPrescription && <LinearGradient colors={['#ECE7F6', '#EBE8F8', '#E4EFFF', '#FAF1E2']} style={{ flex: 1 }} start={{ x: 1, y: 0 }} end={{ x: 0, y: 1 }} >
-                <Container bg='transparent'>
-                    <MainPageHeader />
-                    <View className='flex-1 justify-center items-center'>
-                        <View
-                            className='bg-white border border-[#E8F3FF] rounded-xl py-6 px-7  justify-center items-center'
-                        >
-                            <Image source={img_prescription_empty} className='w-20 h-20' />
-                            <View className='mt-6 gap-3'>
-                                <Text className='text-center text-[#101828] font-semiBoldSFDisplay text-base'>
-                                    No Prescription Yet
-                                </Text>
-                                <Text className='text-center font-regularSFDisplay text-base text-[#667085]'>
-                                    All prescriptions would appear here once they are updated
-                                </Text>
-                            </View>
-                            <View className='mt-5'>
-                                <AddButton  onPress={handleClick}/>
-                            </View>
-                        </View>
+	const handleClick = () => {
+		router.push("/stepOne");
+	};
+	return (
+		<>
+			{!clickPrescription && (
+				<LinearGradient
+					colors={["#ECE7F6", "#EBE8F8", "#E4EFFF", "#FAF1E2"]}
+					style={{ flex: 1 }}
+					start={{ x: 1, y: 0 }}
+					end={{ x: 0, y: 1 }}
+				>
+					<Container bg="transparent">
+						<MainPageHeader />
+						<View className="flex-1 justify-center items-center">
+							<View className="bg-white border border-[#E8F3FF] rounded-xl py-6 px-7  justify-center items-center">
+								<Image source={img_prescription_empty} className="w-20 h-20" />
+								<View className="mt-6 gap-3">
+									<Text className="text-center text-[#101828] font-semiBoldSFDisplay text-base">
+										No Prescription Yet
+									</Text>
+									<Text className="text-center font-regularSFDisplay text-base text-[#667085]">
+										All prescriptions would appear here once they are updated
+									</Text>
+								</View>
+								<View className="mt-5">
+									<AddButton onPress={handleClick} />
+								</View>
+							</View>
+						</View>
+					</Container>
+				</LinearGradient>
+			)}
+			{clickPrescription && (
+				<Container bg={"white"} px="px-0">
+					<LinearGradient
+						colors={["#E0EDFF", "#E4EFFF", "#E7F0FF"]}
+						style={{ flex: 1 }}
+						start={{ x: 0, y: 0 }}
+						end={{ x: 0, y: 1 }}
+					>
+						<View className="flex-1 px-6">
+							<MainPageHeader />
+							<View>
+								<Text className="font-mediumSFDisplay text-base">
+									Verify your prescription in less than a minute
+								</Text>
+								<View className="mt-[10px]">
+									<AddButton text="Check Prescriptions" onPress={handleClick} />
+								</View>
+							</View>
+							<View className="mt-5">
+								<Text className="font-boldSFDisplay text-base leading-[32px]">
+									Recent Prescriptions
+								</Text>
+								<View className="gap-5 mt-4">
+									{recentPrescriptions.map((prescription, index) => {
+										return (
+											<RecentPrescriptionCard
+												key={index}
+												name={prescription.patient_name}
+												illness={prescription.patient_illness}
+												medication={prescription.patient_medication}
+												date={prescription.created_at}
+												rating={prescription.rating}
+											/>
+										);
+									})}
+								</View>
+							</View>
+						</View>
+					</LinearGradient>
+				</Container>
+			)}
+		</>
+	);
+};
 
-                    </View>
-                </Container>
-            </LinearGradient>
-            }
-            {
-                clickPrescription && <Container bg={'white'} px='px-0'>
-                    <LinearGradient colors={['#E0EDFF', '#E4EFFF', '#E7F0FF']} style={{ flex: 1 }} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} >
-                        <View className='flex-1 px-6'>
-                            <MainPageHeader />
-                            <View>
-                                <Text className='font-mediumSFDisplay text-base'>
-                                    Verify your prescription in less than a minute
-                                </Text>
-                                <View className='mt-[10px]'>
-                                    <AddButton text='Check Prescriptions' onPress={handleClick} />
-                                </View>
-                            </View>
-                            <View className='mt-5'>
-                                <Text className='font-boldSFDisplay text-base leading-[32px]'>
-                                    Recent Prescriptions
-                                </Text>
-                                <View className='gap-5 mt-4'>
-                                    {
-                                        recentPrescriptions.map((prescription, index) => {
-                                            return <RecentPrescriptionCard
-                                                key={index}
-                                                name={prescription.patient_name}
-                                                illness={prescription.patient_illness}
-                                                medication={prescription.patient_medication}
-                                                date={prescription.created_at}
-                                                rating={prescription.rating}
-                                            />
-                                        })
-                                    }
-                                </View>
-                            </View>
-                        </View>
-
-                    </LinearGradient>
-
-                </Container>
-            }
-        </>
-    )
-}
-
-export default PrescriptionOverviewScreen
+export default PrescriptionOverviewScreen;
